71d878ac3382125a6cfb2f58c11c1d5cdb8f274f
Hunter Austin <htaust01@gmail.com>
2025-03-19 19:20:39 -0400

    added changePic and isValidGuess functions

--- public/js/playgame.js
+++ public/js/playgame.js
@@ -1,13 +1,16 @@
 "use strict";
 
-const image = document.getElementById("gamePic");
+const gamePic = document.getElementById("gamePic");
 const submitBtn = document.getElementById("submitBtn")
 const wordDisplay = document.getElementById("word");
+const feedback = document.getElementById("feedback");
 
 let currentWord = "HANGMAN";
 let wordArr = [];
 let guessArr = [];
 let prevGuesses = [];
+let currentImage = 1;
+let usedLetters = [];
 
 for(let i = 0; i < currentWord.length; i++)
 {
@@ -16,3 +19,16 @@ for(let i = 0; i < currentWord.length; i++)
 }
 
 wordDisplay.innerText = guessArr.join(" ");
+
+const changePic = () => {
+   currentImage = currentImage === 8 ? 1 : currentImage + 1;
+   gamePic.src = `./imgs/hangman0${currentImage}.png`
+}
+
+const isValidGuess = guess => guess.length === 1 && "ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(guess.toUpperCase());
+
+const makeGuess = () => {
+
+}
+
+submitBtn.addEventListener("click", changePic);
